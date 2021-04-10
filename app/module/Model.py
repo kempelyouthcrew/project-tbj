81b3ea8bdeaaa6b50eb59147398a48e42b8b8ba5
kempelyouthcrew <73091098+kempelyouthcrew@users.noreply.github.com>
2021-04-10 15:39:05 +0700

    Modify-Model

--- app/module/Model.py
+++ app/module/Model.py
@@ -3,10 +3,104 @@ from app import app
 
 db = SQLAlchemy(app)
 
-class Mahasiswa(db.Model):
-    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
-    nim = db.Column(db.String(100), nullable=False)
-    nama = db.Column(db.String(100), nullable=False)
+# SupplierDB, SparepartDetail, SparepartDB, QuotationDB, QuotationDetail, KonsumenDB, DODB, PODB, PODetail
 
-    def __repr__(self):
-        return "<Nama: {}>".format(self.nama)
+class SupplierDB(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    supplier_name = db.Column(db.String(255), nullable=False)
+    supplier_alamat = db.Column(db.String(255), nullable=False)
+    supplier_phone = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class SparepartDetail(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    supplier_name = db.Column(db.String(255), nullable=False)
+    sparepart_number = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class SparepartDB(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    sparepart_name = db.Column(db.String(255), nullable=False)
+    sparepart_number = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class QuotationDB(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    quotation_date = db.Column(db.DateTime, nullable=False)
+    konsumen_id = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class QuotationDetail(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    quotation_number = db.Column(db.Integer, nullable=False)
+    sparepart_number = db.Column(db.Integer, nullable=False)
+    sparepart_qty = db.Column(db.Integer, nullable=False)
+    sparepart_price = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class KonsumenDB(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    konsumen_id = db.Column(db.Integer, nullable=False)
+    konsumen_name = db.Column(db.String(255), nullable=False)
+    konsumen_address = db.Column(db.String(255), nullable=False)
+    konsumen_phone = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class DODB(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    do_number = db.Column(db.Integer, nullable=False)
+    do_date = db.Column(db.DateTime, nullable=False)
+    sparepart_name = db.Column(db.String(255), nullable=False)
+    sparepart_number = db.Column(db.Integer, nullable=False)
+    sparepart_qty = db.Column(db.Integer, nullable=False)
+    sparepart_brand = db.Column(db.String(255), nullable=False)
+    delivery_date = db.Column(db.DateTime, nullable=False)
+    sender_name = db.Column(db.String(255), nullable=False)
+    receiver_nama = db.Column(db.String(255), nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class PODB(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    po_date = db.Column(db.DateTime, nullable=False)
+    seller_nama = db.Column(db.String(255), nullable=False)
+    konsumen_name = db.Column(db.String(255), nullable=False)
+    po_number = db.Column(db.Integer, nullable=False)
+    po_reference_number = db.Column(db.Integer, nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
+
+class PODetail(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True, nullable=False)
+    po_date = db.Column(db.DateTime, nullable=False)
+    sparepart_name = db.Column(db.String(255), nullable=False)
+    sparepart_qty = db.Column(db.Integer, nullable=False)
+    konsumen_id = db.Column(db.Integer, nullable=False)
+    sparepart_price = db.Column(db.Integer, nullable=False)
+    sparepart_brand = db.Column(db.String(255), nullable=False)
+    created_by = db.Column(db.String(255), nullable=True)
+    updated_by = db.Column(db.String(255), nullable=True)
+    created_add = db.Column(db.DateTime, nullable=True)
+    updated_add = db.Column(db.DateTime, nullable=True)
--- app/module/controller.py
+++ app/module/controller.py
@@ -7,56 +7,65 @@
 
 from flask import render_template, request, redirect
 from app import app
-from .Model import db, Mahasiswa
+from .Model import db, SupplierDB, SparepartDetail, SparepartDB, QuotationDB, QuotationDetail, KonsumenDB, DODB, PODB, PODetail
 
 @app.route('/dashboard', methods=['GET'])
 def test():
     return render_template("dashboard.html")
 
-@app.route('/', methods=['GET','POST'])
-def index():
+@app.route('/input-konsumen', methods=['GET','POST'])
+def input_konsumen():
     if request.method == 'POST':
-        nama = request.form['nama']
-        nim = request.form['nim']
+        konsumen_id = request.form['konsumen_id']
+        konsumen_name = request.form['konsumen_name']
+        konsumen_address = request.form['konsumen_address']
+        konsumen_phone = request.form['konsumen_phone']
         try:
-            mhs = Mahasiswa(nim=nim, nama=nama)
-            db.session.add(mhs)
+            konsumen = KonsumenDB(konsumen_id=konsumen_id, 
+                                konsumen_nama=konsumen_nama, 
+                                konsumen_address=konsumen_address, 
+                                konsumen_phone=konsumen_phone)
+            db.session.add(konsumen)
             db.session.commit()
         except Exception as e:
             print("Failed to add data.")
             print(e)
-    listMhs = Mahasiswa.query.all()
-    print(listMhs)
-    return render_template("home.html", data=enumerate(listMhs,1))
+    listkonsumen = KonsumenDB.query.all()
+    print(listkonsumen)
+    return render_template("home.html", data=enumerate(listkonsumen,1))
 
-@app.route('/form-update/<int:id>')
-def updateForm(id):
-    mhs = Mahasiswa.query.filter_by(id=id).first()
-    return render_template("update.html", data=mhs)
+@app.route('/update-konsumen/<int:id>')
+def updateform_konsumen(id):
+    konsumen = KonsumenDB.query.filter_by(id=id).first()
+    return render_template("update.html", data=konsumen)
 
-@app.route('/form-update', methods=['POST'])
-def update():
+@app.route('/update-konsumen', methods=['POST'])
+def update_konsumen():
     if request.method == 'POST':
         id = request.form['id']
-        nama = request.form['nama']
-        nim = request.form['nim']
+        konsumen_id = request.form['konsumen_id']
+        konsumen_name = request.form['konsumen_name']
+        konsumen_address = request.form['konsumen_address']
+        konsumen_phone = request.form['konsumen_phone']
         try:
-            mhs = Mahasiswa.query.filter_by(id=id).first()
-            mhs.nama = nama
-            mhs.nim = nim
+            konsumen = Mahasiswa.query.filter_by(id=id).first()
+            konsumen.konsumen_id=konsumen_id
+            konsumen.konsumen_nama=konsumen_nama
+            konsumen.konsumen_address=konsumen_address
+            konsumen.konsumen_phone=konsumen_phone
             db.session.commit()
         except Exception as e:
             print("Failed to update data")
             print(e)
-        return redirect("/")
+        return redirect("/input-konsumen")
 
-@app.route('/delete/<int:id>')
-def delete(id):
+@app.route('/delete-konsumen/<int:id>')
+def delete_konsumen(id):
     try:
-        mhs = Mahasiswa.query.filter_by(id=id).first()
-        db.session.delete(mhs)
+        konsumen = KonsumenDB.query.filter_by(id=id).first()
+        db.session.delete(konsumen)
         db.session.commit()
     except Exception as e:
-        print("Failed delete mahasiswa")
+        print("Failed to delete data")
         print(e)
-    return redirect("/")
+    return redirect("/input-konsumen")
