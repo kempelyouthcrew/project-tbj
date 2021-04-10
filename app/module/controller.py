3c1109a5dc4dd7d77ba74d859ea40565d0b6c668
bayuyuhartono-katadata <bayu.yuhartono@katadata.co.id>
2021-04-10 18:27:45 +0700

    konsumen

--- app/module/controller.py
+++ app/module/controller.py
@@ -36,6 +36,64 @@ def konsumen():
     print(listData)
     return render_template("sites/konsumen/index.html", data=enumerate(listData,1))
 
+@app.route('/konsumen/add', methods=['GET'])
+def konsumenAddForm():
+    return render_template("sites/konsumen/addForm.html")
+
+@app.route('/konsumen/add', methods=['POST'])
+def konsumenAdd():
+    konsumen_id = request.form['konsumen_id']
+    konsumen_name = request.form['konsumen_name']
+    konsumen_address = request.form['konsumen_address']
+    konsumen_phone = request.form['konsumen_phone']
+    try:
+        konsumen = KonsumenDB(konsumen_id=konsumen_id, 
+                            konsumen_name=konsumen_name, 
+                            konsumen_address=konsumen_address, 
+                            konsumen_phone=konsumen_phone)
+        db.session.add(konsumen)
+        db.session.commit()
+    except Exception as e:
+        print("Failed to add data.")
+        print(e)
+
+    return render_template("sites/konsumen/addForm.html")
+
+@app.route('/konsumen/edit/<int:id>', methods=['GET'])
+def konsumenEditForm(id):
+    konsumen = KonsumenDB.query.filter_by(id=id).first()
+    return render_template("sites/konsumen/editForm.html", data=konsumen)
+
+@app.route('/konsumen/edit', methods=['POST'])
+def konsumenEdit():
+    id = request.form['id']
+    konsumen_id = request.form['konsumen_id']
+    konsumen_name = request.form['konsumen_name']
+    konsumen_address = request.form['konsumen_address']
+    konsumen_phone = request.form['konsumen_phone']
+    try:
+        konsumen = KonsumenDB.query.filter_by(id=id).first()
+        konsumen.konsumen_id=konsumen_id
+        konsumen.konsumen_name=konsumen_name
+        konsumen.konsumen_address=konsumen_address
+        konsumen.konsumen_phone=konsumen_phone
+        db.session.commit()
+    except Exception as e:
+        print("Failed to update data")
+        print(e)
+    return redirect("/konsumen")
+
+@app.route('/konsumen/delete/<int:id>')
+def deleteKonsumen(id):
+    try:
+        konsumen = KonsumenDB.query.filter_by(id=id).first()
+        db.session.delete(konsumen)
+        db.session.commit()
+    except Exception as e:
+        print("Failed to delete data")
+        print(e)
+    return redirect("/konsumen")
+
 @app.route('/sparepart', methods=['GET'])
 def sparepart():
     return render_template("sites/konsumen/index.html")
